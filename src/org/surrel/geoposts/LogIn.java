488955eac81a5581eda2b402e983f4179d132fd9
gsurrel <gregoire@surrel.org>
2014-01-15 08:58:08 +0100

    Changed entry point of app to get the right name

--- src/org/surrel/geoposts/LogIn.java
+++ src/org/surrel/geoposts/LogIn.java
@@ -6,34 +6,18 @@ import java.util.concurrent.TimeoutException;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class LogIn extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// We check if user already logged in
-		Log.v("Act.login", "Autologin?");
-		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-		if(settings.getInt("user_id", -1) == -1)
-		{
-			Log.v("Act.login", "LOL, no!");
-			setContentView(R.layout.activity_login);
-		}
-		else
-		{
-			Log.v("Act.login", "Yes, sir!");
-			Intent toMain = new Intent(this, MainActivity.class);
-			startActivity(toMain);
-		}
+		setContentView(R.layout.activity_login);
 	}
 
 	@Override
--- src/org/surrel/geoposts/MainActivity.java
+++ src/org/surrel/geoposts/MainActivity.java
@@ -1,7 +1,5 @@
 package org.surrel.geoposts;
 
-import org.json.JSONObject;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -58,38 +56,51 @@ public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		// Test if Google Play Services installed
-		int gps = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
-		CharSequence text = "";
-		int duration = Toast.LENGTH_LONG;
-		switch(gps){
-		case ConnectionResult.SUCCESS:
-			text = "Google Play Services loaded";
-			duration = Toast.LENGTH_SHORT;
-			break;
-		case ConnectionResult.SERVICE_MISSING:
-			text = "Google Play Services missing";
-			break;
-		case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
-			text = "Google Play Services needs an update";
-			break;
-		case ConnectionResult.SERVICE_DISABLED:
-			text = "Google Play Services disabled";
-			break;
-		case ConnectionResult.SERVICE_INVALID:
-			text = "Google Play Services invalid";
-			break;
-		case ConnectionResult.DATE_INVALID:
-			text = "Google Play Services date invalid";
-			break;
-		default:
-			text = "Unknown result";
-			break;
+
+		// We check if user already logged in
+		Log.v("Act.login", "Autologin?");
+		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		if(settings.getInt("user_id", -1) != -1)
+		{
+			Log.v("Act.login", "Yes, sir!");
+			setContentView(R.layout.activity_main);
+			// Test if Google Play Services installed
+			int gps = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+			CharSequence text = "";
+			int duration = Toast.LENGTH_LONG;
+			switch(gps){
+			case ConnectionResult.SUCCESS:
+				text = "Google Play Services loaded";
+				duration = Toast.LENGTH_SHORT;
+				break;
+			case ConnectionResult.SERVICE_MISSING:
+				text = "Google Play Services missing";
+				break;
+			case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
+				text = "Google Play Services needs an update";
+				break;
+			case ConnectionResult.SERVICE_DISABLED:
+				text = "Google Play Services disabled";
+				break;
+			case ConnectionResult.SERVICE_INVALID:
+				text = "Google Play Services invalid";
+				break;
+			case ConnectionResult.DATE_INVALID:
+				text = "Google Play Services date invalid";
+				break;
+			default:
+				text = "Unknown result";
+				break;
+			}
+			Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+			toast.show();
 		}
-		Toast toast = Toast.makeText(getApplicationContext(), text, duration);
-		toast.show();
+		else
+		{
+			Log.v("Act.login", "LOL, no, back to login!");
+			Intent toLogin = new Intent(this, LogIn.class);
+			startActivity(toLogin);
+		}
 	}
 
 	public void ViewMap(View view) {
